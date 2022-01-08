e03b5817c08ced9b4a9a9d713e2ecdced5e48b5b
poplink <2219535001@qq.com>
2022-01-08 23:58:19 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unsed-vars': 'off',
+    'no-unused-vars': 'off',
+    'no-emtpy': 'off',
+    'vue/no-unused-components': 'off',
+  }
+}
